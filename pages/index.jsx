ee9e6a9c5fa19fc1bed8622dbb103014add3dcba
Yeison Montoya <yeisondiseno@gmail.com>
2021-09-26 23:08:46 -0500

    Y.Montoya: no type

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react"
 // components
 import Navbar from "../components/Navbar/Navbar"
 
 const Home = () => {
+  // data
+  const [ productList, setProductList ] = useState([])
+  
+  console.log('productList', productList)
+  useEffect(() => {
+    fetch('api/avo')
+      .then(res => res.json())
+      .then( ({data, length }) => {
+        setProductList(data) 
+      })
+      .catch( error => console.error('Este es error', error) )
+  }, [])
+
   return(
     <div >
       <Navbar />
       <h1>
         Hola Mundo adwdawd ad 
       </h1>
+      {
+        productList.length > 0 && productList.map( product => 
+          <div key={product.id} >{product.name}</div>
+        )
+      }
     </div>
   )
 }
--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react"
+// router
 import { useRouter } from 'next/router'
 
 const ProductItem = () => {
-
+  // route
   const { query: { id } } = useRouter()
+
+  // data
+  const [ product, setProduct ] = useState([])
+
+  console.log('product', product, 'id', id)
+
+  useEffect(() => {
+    if(!id) return
+    fetch(`/api/avo/${id}`)
+      .then(res => res.json())
+      .then( data => setProduct(data))
+      .catch( error => console.error('Este es error', error) )
+  }, [id])
 
   return(
     <div>
-      Hola  soy un producto: {id}
+      <h4>
+        Hola  soy un producto: {id}
+      </h4>
+      <div>
+        {product?.name}
+      </div>
     </div>
   )
 } 
