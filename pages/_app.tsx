2ecac0cbb8fe22a528301837cb752fcb2b507346
Yeison Montoya <yeisondiseno@gmail.com>
2021-10-09 12:36:47 -0500

    Y.Montoya: get metrics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,11 @@ import '../global.css'
 
 import CartProvider from '@store/Cart'
 
+// performance report 
+export function reportWebVitals (metric: any) {
+  console.log('metric', metric)
+}
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   // Aditional props
   // Aditional layout
